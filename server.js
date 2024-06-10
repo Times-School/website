29525dec86aec168a71805892dcf264b5390d683
Times-School <timesschoollc@gmail.com>
2024-06-10 19:44:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.post('/submit-form', (req, res) => {
     const formData = req.body;
 
-    // Send email
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'timesschoollc@gmail.com',
-            pass: 'iaky jacs yzof qpzx' // Use the generated app password here
+            pass: 'iaky jacs yzof qpzx'
         }
     });
 
     const mailOptions = {
         from: 'timesschoollc@gmail.com',
-        to: 'timesschoollc@gmail.com', // Emailing to yourself
+        to: 'timesschoollc@gmail.com',
         subject: 'New Demo Lesson Request',
         text: `Name: ${formData.name}\nPhone: ${formData.phone}\nCourse: ${formData.course}\nPreferred Time: ${formData.preferred_time}`
     };
@@ -30,6 +33,11 @@ app.post('/submit-form', (req, res) => {
         }
         res.redirect('/thank-you.html');
     });
+});
+
+// Serve thank-you.html
+app.get('/thank-you.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'thank-you.html'));
 });
 
 app.listen(3000, () => {
